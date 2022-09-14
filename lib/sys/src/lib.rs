7067c5d8d5c63ecf1ff2002f2d46d6c5ed6a2988
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-09-14 11:41:25 +0200

    Ignore formatting for generated code

--- lib/sys/src/lib.rs
+++ lib/sys/src/lib.rs
@@ -1,6 +1,7 @@
 #[allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
 #[allow(clippy::all)]
 #[allow(rustdoc::broken_intra_doc_links)]
+#[rustfmt::skip]
 #[path = "../generated/sys.rs"]
 mod sys;
 pub use sys::*;
